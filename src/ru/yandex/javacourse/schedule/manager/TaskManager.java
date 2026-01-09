6d63f4d4758ab09a5ec69a4234d594087caf4d54
Makanin Yaroslav <fobosguychanel@gmail.com>
2026-01-09 17:45:51 +0300

    chore: unused import in TaskManager

--- src/ru/yandex/javacourse/schedule/manager/TaskManager.java
+++ src/ru/yandex/javacourse/schedule/manager/TaskManager.java
@@ -1,7 +1,6 @@
 package ru.yandex.javacourse.schedule.manager;
 
 import java.util.List;
-import java.util.Optional;
 
 import ru.yandex.javacourse.schedule.tasks.Epic;
 import ru.yandex.javacourse.schedule.tasks.Subtask;
